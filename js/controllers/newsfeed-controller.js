50bb2c0907e1a10c2139aa3e213ccc2666091935
Sophie Mann <sklm@live.co.uk>
2017-05-26 11:21:07 +0100

    adds function to controller to populate page

--- js/controllers/newsfeed-controller.js
+++ js/controllers/newsfeed-controller.js
@@ -4,5 +4,10 @@ function Controller(view = new View()) {
   this.view = view;
 }
 
+Controller.prototype.populatePage = function(guardianResponse) {
+  element = document.getElementById("headlines");
+  element.innerHTML = this.view.getHTML(guardianResponse);
+};
+
 exports.Controller = Controller;
 })(this);
--- spec/newsfeed-controller-spec.js
+++ spec/newsfeed-controller-spec.js
@@ -4,3 +4,13 @@ function testControllerInitialisesWithView(){
   assert.isConstructedFrom(controller.view, View);
 }
 testControllerInitialisesWithView();
+
+function testControllerPopulatesPage(){
+  console.log('Test controller populates page:');
+  var controller = new Controller();
+  var response = [{"response":{"results":[{"webTitle":"Todays Headline"}]}}]
+  controller.view.newsfeed.addHeadlines(response);
+  var html = "<ul><li><div>Todays Headline</div></li></ul>";
+  assert.isEqual(controller.view.getHTML(), html);
+}
+testControllerPopulatesPage();
