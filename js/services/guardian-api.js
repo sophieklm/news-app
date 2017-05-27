dc23dcff677324146d78de81310190a4c5dff5ae
Sophie Mann <sklm@live.co.uk>
2017-05-27 12:10:53 +0100

    refactors api response functions to include summary

---
+++ js/services/guardian-api.js
@@ -0,0 +1,21 @@
+function getResponse(method) {
+  var newResponse = [];
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    if(request.readyState === 4 && request.status === 200) {
+      newResponse.push(JSON.parse(request.responseText));
+      }
+  };
+  request.open('GET', method);
+  request.send(null);
+  return newResponse;
+}
+
+function getHeadlines() {
+  return "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?"
+}
+
+function getSummary(id){
+  var url = controller.newsfeed.getUrl(id);
+  return "http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=" + url
+}
