ca712c4f87188d86e3f50d2489083916891c87be
Sophie Mann <sklm@live.co.uk>
2017-05-27 21:13:13 +0100

    refactors to use callback instead of timeout

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 function init() {
 
-  var guardianResponse = getResponse(getHeadlines());
-  setTimeout(function() {
-    controller = new Controller(guardianResponse);
+  var url = "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?show-fields=thumbnail"
+  var newResponse = []
+
+  function getResponse(url, callback) {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+      if(request.readyState === 4 && request.status === 200) {
+        newResponse.push(JSON.parse(request.responseText));
+        callback.call(newResponse);
+        }
+    };
+    request.open('GET', url);
+    request.send(null);
+    return newResponse;
+  }
+
+  getResponse(url, function() {
+    controller = new Controller(newResponse);
     controller.populatePage();
-  },1500);
+  });
+
 }
 
 window.onload = init;
