1944014f00cbdfca3de87ab5868ffe7efdbf8232
Sophie Mann <sklm@live.co.uk>
2017-05-27 12:08:34 +0100

    adds show summary function to controller

--- js/controllers/newsfeed-controller.js
+++ js/controllers/newsfeed-controller.js
@@ -1,13 +1,24 @@
 (function(exports){
 
-function Controller(view = new View()) {
-  this.view = view;
+function Controller(guardianResponse) {
+  this.newsfeed = new Newsfeed(guardianResponse);
+  this.view = new View(this.newsfeed);
 }
 
-Controller.prototype.populatePage = function(guardianResponse) {
+Controller.prototype.populatePage = function() {
   element = document.getElementById("headlines");
-  element.innerHTML = this.view.getHTML(guardianResponse);
+  element.innerHTML = this.view.getHTML(this.newsfeed);
 };
 
 exports.Controller = Controller;
 })(this);
+
+
+function showSummary(id) {
+  var article = new Article(id);
+  setTimeout(function() {
+    var summaryview = new SummaryView(article);
+    element = document.getElementById('summary');
+    element.innerHTML = summaryview.getSummaryHTML();
+  },2500);
+}
